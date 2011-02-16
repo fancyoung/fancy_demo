a5836601ab67771dcfaa1d397f631f3e0b56758e
fancyoung <siyang1982@msn.com>
2011-02-16 17:25:53 +0800

    run $ rails new fancy_demo --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FancyDemo::Application.config.secret_token = 'ce9f6d694bcbdf0698174a7e0e981922ccc2ae9441e5e37f0a2f483d6a5594025fb201f57f9789890d64edb13e78093af8ff499dd32627926deb99e5e955a635'
