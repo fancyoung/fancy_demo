9d71b10d3050c6af6446416786b44ceda2ea5750
fancyoung <siyang1982@msn.com>
2011-02-16 22:32:27 +0800

    add name for user.(I do not know why name can not be set as unique in model)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  field :name, :type => String
+  
+  index :name
+  
+  validates_length_of :name, :minimum => 2, :maximum => 30
 end
